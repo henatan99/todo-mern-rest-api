60d2e49ef964a68d5b9f7e5821aba3d7aacd9487
Henok Mekonnen <henatan99@yahoo.com>
2021-10-20 13:11:56 +0300

    Setup server in index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+// we are using pport 8000
+const port = 8000;
+
+
+// we will create these todoRoutes in the future
+const todoRoutes = require('./routes/Todo');
+
+const app = express();
+
+// DB connection
+
+mongoose.connect('mongodb://127.0.0.1:27017//todoapp', {
+    useNewUrlParser: true,
+    useUnifiedTopoplogy: true,
+    useCreateIndex: true,
+})
+.then(() => {
+    console.log('CONNECTED TO DATABASE');
+});
+
+// middleware for cors to allow cross origin resource sharing
+app.use(cors());
+
+// middleware to convert our request data inot JSON format
+app.use(bodyParser.json());
+
+// include the todoRoutes
+app.use('/api', todoRoutes);
+
+// start the server in the port 8080
+app.listen(port, () => {
+    console.log(`Listening to http://localhost:${port}`);
+});
