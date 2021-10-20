339aa6bf78ffd8c7c155ae261aba56ec838d510d
Henok Mekonnen <henatan99@yahoo.com>
2021-10-20 13:33:27 +0300

    Add getAllTodos controller in Todo controllers

--- controllers/Todo.js
+++ controllers/Todo.js
@@ -19,3 +19,18 @@ exports.getTodoById = (req, res, next, todoId) => {
     });
 };
 
+exports.getAllTodos = (req, res) => {
+    // simply use .find() method and it will return all teh todos
+    Todo.find()
+    .sort('-createdAt')
+    .exec((err, todos) => {
+        //error checking
+        if (err || !todos) {
+            return res.status(400).json({
+                error: 'Something went wrong in finding all todos',
+            });
+        }
+        // return all the todos in json format
+        res.json(todos);
+    })
+};
